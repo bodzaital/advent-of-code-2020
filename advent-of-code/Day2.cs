859f1202053c520ac6dc2b007bb3f2b0e56a4a07
Zsolt Boda <boda0zsolt@live.com>
2020-12-02 11:16:56 +0100

    Day 2 solutions

---
+++ advent-of-code/Day2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace advent_of_code
+{
+    public static class Day2
+    {
+        public static void Part1()
+        {
+            string[] list = File.ReadAllLines("day2-input.txt");
+
+            int correct_passwords = 0;
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                string[] line_split = list[i].Split(':');
+                string[] rule_split = line_split[0].Split(' ');
+                string[] occurences = rule_split[0].Split('-');
+
+                int minimum = int.Parse(occurences[0]);
+                int maximum = int.Parse(occurences[1]);
+
+                char character = rule_split[1][0];
+
+                string password = line_split[1].Trim();
+
+                int real_occurence = password.Count(x => x == character);
+
+                if (real_occurence >= minimum && real_occurence <= maximum)
+                {
+                    correct_passwords++;
+                }
+            }
+
+            Console.WriteLine(correct_passwords);
+        }
+
+        public static void Part2()
+        {
+            string[] list = File.ReadAllLines("day2-input.txt");
+
+            int correct_passwords = 0;
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                string[] line_split = list[i].Split(':');
+                string[] rule_split = line_split[0].Split(' ');
+                string[] occurences = rule_split[0].Split('-');
+
+                int position1 = int.Parse(occurences[0]);
+                int position2 = int.Parse(occurences[1]);
+
+                char character = rule_split[1][0];
+
+                string password = line_split[1].Trim();
+
+                if (password[position1 - 1] == character ^ password[position2 - 1] == character)
+                {
+                    correct_passwords++;
+                }
+            }
+
+            Console.WriteLine(correct_passwords);
+        }
+    }
+}
--- advent-of-code/Program.cs
+++ advent-of-code/Program.cs
@@ -7,7 +7,9 @@ namespace advent_of_code
         static void Main(string[] args)
         {
             //Day1.Part1();
-            Day1.Part2();
+            //Day1.Part2();
+            //Day2.Part1();
+            Day2.Part2();
         }
     }
 }
