dde2585fbf428409e1a426eb203bbcc0f42b27cd
Zsolt <5047426+bodzaital@users.noreply.github.com>
2020-12-07 22:04:32 +0100

    Day 6 setup

---
+++ advent-of-code/Day6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace advent_of_code
+{
+    public static class Day6
+    {
+        public static void Part1()
+        {
+
+        }
+
+        public static void Part2()
+        {
+
+        }
+    }
+}
